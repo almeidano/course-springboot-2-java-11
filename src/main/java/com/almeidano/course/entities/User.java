d8f7ee60804e684aaf953a304c6c689928a31bac
Anderson Almeida <almeida.oliveira@uol.com.br>
2020-11-17 12:16:25 -0300

    H2 database, test profile, JPA

--- src/main/java/com/almeidano/course/entities/User.java
+++ src/main/java/com/almeidano/course/entities/User.java
@@ -2,9 +2,17 @@ package com.almeidano.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable { //tem que haver um numero de serie em User
 	private static final long serialVersionUID = 1L;
-		
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
